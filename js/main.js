3e776669470a1ce9d256adf41d38b7a574bcf692
Feldbush <feldbush@live.ru>
2020-10-13 21:19:36 +0300

    1. Исправил нейминг констант 2.Убрал функцию корректировки позиционирования, теперь отталкиваюьс от размеров аватарки пользователя на пине

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 'use strict';
 
-const quantityPins = 8;
-const titlesOffer = [`Супер классный, новый, модный отель`, `Вот это отель`, `Гостиница так гостиница`];
-const descriptions = [`Лучший отель на этой планете Лучший отель на этой планете Лучший отель на этой планете`, `Бомба БомбаБомбаБомбаБомбаБомбаБомба`, `Шикарно Шикарно Шикарно Шикарно`];
-const offerType = [`palace`, `flat`, `house`, `bungalow`];
-const maxQuantityRooms = 5;
-const maxQuantityGuest = 7;
-const checkinTimes = [`12:00`, `13:00`, `14:00`];
-const checkoutTimes = [`12:00`, `13:00`, `14:00`];
-const features = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-const photosLinks = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const QUANTITY_PINS = 8;
+const TITLE_OFFER = [`Супер классный, новый, модный отель`, `Вот это отель`, `Гостиница так гостиница`];
+const DESCRIPTION = [`Лучший отель на этой планете Лучший отель на этой планете Лучший отель на этой планете`, `Бомба БомбаБомбаБомбаБомбаБомбаБомба`, `Шикарно Шикарно Шикарно Шикарно`];
+const OFFER_TYPE = [`palace`, `flat`, `house`, `bungalow`];
+const MAX_QUANTITY_ROOMS = 5;
+const MAX_QUANTITY_GUEST = 7;
+const CHECKIN_TIMES = [`12:00`, `13:00`, `14:00`];
+const CHECKOUT_TIMES = [`12:00`, `13:00`, `14:00`];
+const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const PHOTOS_LINKS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 const mapNode = document.querySelector(`.map`);
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
-const pinLocation = {
+const PIN_LOCATION = {
   yMin: 130,
   yMax: 630,
   xMin: 50,
   xMax: mapNode.offsetWidth - 100
 };
-
 
 function getRandomInteger(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
@@ -39,20 +38,20 @@ function createMockPinData(count) {
         avatar: `img/avatars/user0${index + 1}.png`
       },
       offer: {
-        title: getRandomElementArray(titlesOffer),
+        title: getRandomElementArray(TITLE_OFFER),
         price: getRandomInteger(1000, 4500),
-        type: getRandomElementArray(offerType),
-        rooms: getRandomInteger(1, maxQuantityRooms),
-        guests: getRandomInteger(1, maxQuantityGuest),
-        checkin: getRandomElementArray(checkinTimes),
-        checkout: getRandomElementArray(checkoutTimes),
-        features: features.slice(0, getRandomInteger(1, features.length - 1)),
-        description: getRandomElementArray(descriptions),
-        photos: photosLinks.slice(0, getRandomInteger(1, photosLinks.length - 1))
+        type: getRandomElementArray(OFFER_TYPE),
+        rooms: getRandomInteger(1, MAX_QUANTITY_ROOMS),
+        guests: getRandomInteger(1, MAX_QUANTITY_GUEST),
+        checkin: getRandomElementArray(CHECKIN_TIMES),
+        checkout: getRandomElementArray(CHECKOUT_TIMES),
+        FEATURES: FEATURES.slice(0, getRandomInteger(1, FEATURES.length - 1)),
+        description: getRandomElementArray(DESCRIPTION),
+        photos: PHOTOS_LINKS.slice(0, getRandomInteger(1, PHOTOS_LINKS.length - 1))
       },
       location: {
-        x: getRandomInteger(pinLocation.xMin, pinLocation.xMax),
-        y: getRandomInteger(pinLocation.yMin, pinLocation.yMax)
+        x: getRandomInteger(PIN_LOCATION.xMin, PIN_LOCATION.xMax),
+        y: getRandomInteger(PIN_LOCATION.yMin, PIN_LOCATION.yMax)
       }
     };
     mockPinData.offer.address = `${mockPinData.location.x},${mockPinData.location.y}`;
@@ -69,8 +68,8 @@ function createPinNode(pinData) {
   pinImg.src = pinData.author.avatar;
   pinImg.alt = pinData.offer.title;
 
-  pin.style.left = pinData.location.x + `px`;
-  pin.style.top = pinData.location.y + `px`;
+  pin.style.left = pinData.location.x - pinImg.width + `px`;
+  pin.style.top = pinData.location.y - pinImg.height + `px`;
 
   return pin;
 }
@@ -88,16 +87,7 @@ function appendPins(pinsData, placeInsertion) {
     temporaryСontainer.append(createPinNode(pinsData[index]));
   }
 
-  placeInsertion.append(temporaryСontainer.cloneNode(true));
-  const pinCssClass = placeInsertion.lastChild.className;
-
-  // Беру количество вставленных элементов с конца контейнера и фиксирую положение их положение таким образом,
-  // чтобы метка указывала на точку указанную в pinData.location.x, pinData.location.y
-  for (let i = temporaryСontainer.children.length; i >= 1; i--) {
-    let currentElement = placeInsertion.querySelector(`.${pinCssClass}:nth-last-child(${i})`);
-    fixPositiPin(currentElement);
-  }
-
+  placeInsertion.append(temporaryСontainer);
 }
 
 
@@ -105,7 +95,7 @@ function appendPins(pinsData, placeInsertion) {
 mapNode.classList.remove(`map--faded`);
 
 // Генерирую пины и вставляю на страницу
-const mockPinsData = createMockPinData(quantityPins);
+const mockPinsData = createMockPinData(QUANTITY_PINS);
 const mapPinsContainer = document.querySelector(`.map__pins`);
 appendPins(mockPinsData, mapPinsContainer);
 
